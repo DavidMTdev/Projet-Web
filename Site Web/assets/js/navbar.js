6afa7be14d3d0394172a7ff4672c6488ebe43c3a
David Teixeira <david.teixeira31@gmail.com>
2019-03-27 23:13:26 +0100

    Modification de la navbar vertical

---
+++ Site Web/assets/js/navbar.js
@@ -0,0 +1,35 @@
+const menuDelete = document.querySelector('.bar-menu .menuDelete');
+const menuBurger = document.querySelector('.bar-menu .menuBurger');
+const navBarV = document.querySelector('.navbar-v');
+
+menuDelete.addEventListener('click', menu);
+menuBurger.addEventListener('click', menu);
+
+function actionDelete() {
+	navBarV.style.width = '60px';
+	toggleVisibleClassMenuBurger('.bar-menu .menuBurger');
+	toggleVisibleClassMenuBurger('.bar-menu .menuDelete');
+}
+
+function actionMenu() {
+	navBarV.style.width = '250px';
+	toggleVisibleClassMenuBurger('.bar-menu .menuBurger');
+	toggleVisibleClassMenuBurger('.bar-menu .menuDelete');
+}
+
+function toggleVisibleClassMenuBurger(params) {
+	document.querySelector(params).classList.toggle('visible');
+}
+
+var result = 1;
+
+function menu() {
+	if (result == 0) {
+		actionMenu();
+		result = 1;
+	} else {
+		actionDelete();
+		result = 0;
+	}
+	console.log(result);
+}

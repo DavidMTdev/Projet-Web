8c46efcf2742544557404959bb134107ae869ab3
David Teixeira <david.teixeira31@gmail.com>
2019-06-19 19:24:01 +0200

    add page choice on site

---
+++ Site Web/choice.php
@@ -0,0 +1,57 @@
+<?php require_once("include/header.php") ?>
+
+<div class="testimonials">
+    <h1>L'équipe</h1>
+    <div class="border"></div>
+    <div class="inner">
+        <div class="row">
+            <div class="col">
+                <div class="testimonial">
+                    <img src="assets/img/photo cv pro.jpg" alt="">
+                    <div class="name">Jérémy RICHARD</div>
+                    <div class="age">18 ans</div>
+                    <div class="role">Développeur Front </div>
+                </div>
+                <div class="more-button-container">
+                    <a href="home.php?id=1">
+                        <div class="more-button"> En savoir plus <i class="fas fa-arrow-right"></i></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="testimonial">
+                    <img src="assets/img/photo david.PNG" alt="">
+                    <div class="name">David TEIXEIRA</div>
+                    <div class="age">19 ans</div>
+                    <div class="role">Developpeur Back</div>
+                </div>
+                <div class="more-button-container">
+                    <a href="home.php?id=2">
+                        <div class="more-button"> En savoir plus <i class="fas fa-arrow-right"></i></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="testimonial">
+                    <img src="assets/img/Thomaspro.jpg" alt="">
+                    <div class="name">Thomas RUIZ</div>
+                    <div class="age">19 ans</div>
+                    <div class="role">Développeur DataBase</div>
+                </div>
+                <div class="more-button-container">
+                    <a href="home.php?id=3">
+                        <div class="more-button"> En savoir plus <i class="fas fa-arrow-right"></i></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php require_once("include/footer.php") ?>
--- Site Web/include/header.php
+++ Site Web/include/header.php
@@ -8,18 +8,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/navbar.css">
     <link rel="stylesheet" href="assets/css/footer.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
 
-    <?php if ($_SERVER["SCRIPT_NAME"] === "/site web/home.php") : ?>
+    <?php if ($_SERVER["SCRIPT_NAME"] === "/projet-web/site web/home.php") : ?>
+        <link rel="stylesheet" href="assets/css/navbar.css">
         <link rel="stylesheet" href="assets/css/home.css">
-    <?php elseif ($_SERVER["SCRIPT_NAME"] === "/site web/about.php") : ?>
+    <?php elseif ($_SERVER["SCRIPT_NAME"] === "/projet-web/site web/about.php") : ?>
+        <link rel="stylesheet" href="assets/css/navbar.css">
         <link rel="stylesheet" href="assets/css/about.css">
         <link rel="stylesheet" href="assets/css/modifyMenu.css">
-
         <script src="assets/js/about.js" async></script>
         <script src="assets/js/modifyMenu.js" async></script>
+    <?php elseif ($_SERVER["SCRIPT_NAME"] === "/projet-web/site web/choice.php") : ?>
+        <link rel="stylesheet" href="assets/css/choice.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
     <?php endif; ?>
 
 
@@ -27,7 +30,10 @@
 </head>
 
 <body>
-    <header>
-        <?php require_once("include/nav.php") ?>
-    </header>
+    <?php if ($_SERVER["SCRIPT_NAME"] !== "/projet-web/site web/choice.php") : ?>
+        <header>
+            <?php require_once("include/nav.php") ?>
+        </header>
+    <?php endif; ?>
+
     <main>

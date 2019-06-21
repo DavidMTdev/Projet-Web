7e7b7dc9a4066ef50394d5b68646cc26b5e809fa
David Teixeira <david.teixeira31@gmail.com>
2019-06-21 23:23:53 +0200

    remove footer on page choice.php

--- Site Web/include/footer.php
+++ Site Web/include/footer.php
@@ -1,9 +1,13 @@
 </main>
-<footer>
-    <div class="flex center copyright">
-        <p>Copyright © 2019 par DavidMT.</p>
-    </div>
-</footer>
+
+<?php if ($_SERVER["SCRIPT_NAME"] !== "/projet-web/site web/choice.php") : ?>
+    <footer>
+        <div class="flex center copyright">
+            <p>Copyright © 2019 par DavidMT.</p>
+        </div>
+    </footer>
+<?php endif; ?>
+
 </body>
 
 </html>

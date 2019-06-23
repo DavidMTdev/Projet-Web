a3fdd026fca07b613caaec7445b8ba6632e4b61b
David Teixeira <david.teixeira31@gmail.com>
2019-06-23 15:10:38 +0200

    debug timer

--- Site Web/comingsoon.php
+++ Site Web/comingsoon.php
@@ -36,13 +36,14 @@
 </main>
 
 <script type="text/javascript">
-    var count = new Date("sep 24,2019 00:00:00").getTime();
-
     var x = setInterval(function() {
+        var count = new Date("sep 24,2019 00:00:00").getTime();
+
         var now = new Date().getTime();
         var d = count - now;
 
         var days = Math.floor(d / (1000 * 60 * 60 * 24));
+
         var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
         var seconds = Math.floor((d % (1000 * 60)) / (1000));
